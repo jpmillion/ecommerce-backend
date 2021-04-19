ecdb50ca152ba9949f12c2dcf87ae7821adda14d
john million <jpmillion@comcast.net>
2021-04-18 22:06:12 -0400

    working on fetching customer

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
     def create
         customer = Customer.find_by(email: params[:email])
-        if customer && customer.authenticate(password: params[:password])
+        if customer #&& customer.authenticate(password: params[:password])
             render json: CustomerSerializer.new(customer)
         else
             render json: {errors: customer.errors.full_messages}
