28e9ab410e7fd14e798dd0eaf4e5934054aaba3c
john million <jpmillion@comcast.net>
2021-04-25 19:06:03 -0400

    modified orders controller to accept more order item attributes

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::OrdersController < ApplicationController
         customer = Customer.find_by(id: params[:customer_id])
         order = customer.orders.create(order_params)
         params[:order_items].each do |order_item|
-            order.order_items.create(price: order_item[:price], title: order_item[:title], description: order_item[:description])
+            order.order_items.create(price: order_item[:price], title: order_item[:title], description: order_item[:description], quantity: order_item[:quantity], image: order_item[:image])
         end 
         render json: OrderSerializer.new(order)
     end
@@ -11,6 +11,6 @@ class Api::V1::OrdersController < ApplicationController
     private
 
     def order_params
-        params.require(:order).permit(:id, :total)
+        params.require(:order).permit(:total)
     end
 end
