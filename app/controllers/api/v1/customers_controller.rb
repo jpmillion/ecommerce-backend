ef47d73913d884243a7ba56430a29b6c9ecec512
john million <jpmillion@comcast.net>
2021-04-29 19:34:03 -0400

    used bcrypt to hash password from frontend to store in database

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -15,10 +15,12 @@ class Api::V1::CustomersController < ApplicationController
     # end
 
     def create
+        params[:customer][:password_digest] = BCrypt::Password.create(params[:customer][:password_digest])
         customer = Customer.new(customer_params)
         if customer.save
+            cart = customer.create_cart(total: 0)
             token = issue_token(customer)
-            options = {include: [:cart, :cart_items, :order, :order_items]}
+            options = {include: [:cart]}
             render json: { customer: CustomerSerializer.new(customer, options), jwt: token }
         else
             render json: { error: customer.errors.full_messages }, status: :not_acceptable
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
     def create
         customer = Customer.find_by(email: params[:email])
-        if customer #&& customer.authenticate(password: params[:password])
+        if customer && customer.authenticate(params[:password])
             render json: CustomerSerializer.new(customer, {include: [:cart, :orders, :cart_items, :order_items]})
         else
             render json: {errors: customer.errors.full_messages}
         end
     end
+
 end
