416177e2ff8df8f4ca4fc9c0dc1f9a0db6be1e16
john million <jpmillion@comcast.net>
2021-05-02 01:47:33 -0400

    condensed and deleted code from routes and controllers

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,18 +1,4 @@
 class Api::V1::CustomersController < ApplicationController
-    # def create
-    #     customer = Customer.new(customer_params)
-    #     if customer.save
-    #         render json: CustomerSerializer.new(customer)
-    #     else
-    #         render json: {errors: customer.errors.full_messages}
-    #     end
-    # end
-
-    # private
-
-    # def customer_params
-    #     params.require(:customer).permit(:email, :first_name, :last_name, :password)
-    # end
 
     def create
         params[:customer][:password_digest] = BCrypt::Password.create(params[:customer][:password_digest])
--- config/routes.rb
+++ config/routes.rb
@@ -2,39 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html\
   namespace :api do
     namespace :v1 do
-      resources :sessions, only: [:create]
-    end
-  end
 
-  namespace :api do
-    namespace :v1 do
+      resources :sessions, only: [:create]
+
       resources :carts, only: [:update] do
         resources :cart_items, except: [:new, :edit]
       end
-    end
-  end
 
-  namespace :api do
-    namespace :v1 do
       resources :customers, only: [:create] do
+        get 'authenticate', on: :collection
         resources :orders, only: [:create, :index]
       end
-    end
-  end
 
-  namespace :api do
-    namespace :v1 do
       resources :orders, only: [:index] do
         resources :order_items, only: [:create, :index]
       end
-    end
-  end
 
-  namespace :api do
-    namespace :v1 do
-      resources :customers, only: [:create] do
-        get 'authenticate', on: :collection
-      end
     end
   end
 end
