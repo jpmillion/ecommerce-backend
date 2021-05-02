c4bf203e5c78989b5ea3aab88a49084ac9aa652d
john million <jpmillion@comcast.net>
2021-05-01 20:40:22 -0400

    changed params to password digest

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
     def create
         customer = Customer.find_by(email: params[:email])
-        if customer && customer.authenticate(params[:password])
+        if customer && customer.authenticate(params[:password_digest])
             token = issue_token(customer)
             render json: { customer: CustomerSerializer.new(customer), token: token, logIn: true }
         else
